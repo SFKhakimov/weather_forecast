92d3e0ee2909bfbc4aec76d74f9440f39184b088
Shakir Khakimov <s.f.khakimov@gmail.com>
2020-09-04 23:17:13 +0300

    Updates

---
+++ precache-manifest.5762ae05440d4691c8342dfce9c45c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa20bd17f612c3b4f1bbd24c9341198a",
+    "url": "/SFKhakimov/weather_forecast/index.html"
+  },
+  {
+    "revision": "b94c2e674413f5a3889a",
+    "url": "/SFKhakimov/weather_forecast/static/css/main.cfbcba23.chunk.css"
+  },
+  {
+    "revision": "c2df1fe3db3c499e13d4",
+    "url": "/SFKhakimov/weather_forecast/static/js/2.1d445192.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/SFKhakimov/weather_forecast/static/js/2.1d445192.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b94c2e674413f5a3889a",
+    "url": "/SFKhakimov/weather_forecast/static/js/main.c6d7900c.chunk.js"
+  },
+  {
+    "revision": "c982637cc1e46e3eb91e",
+    "url": "/SFKhakimov/weather_forecast/static/js/runtime-main.a98d6d13.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f272ab25002cc544062d432bc272cc1f.js"
+  "/SFKhakimov/weather_forecast/precache-manifest.5762ae05440d4691c8342dfce9c45c0c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SFKhakimov/weather_forecast/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
